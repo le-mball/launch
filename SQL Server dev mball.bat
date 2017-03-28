55685168914f188bbc149f2db139f54a45f1ab1a
Matt Ball <mball@lizearle.com>
2017-03-28 15:43:03 +0100

    Updates sql mssms path

--- SQL Server dev mball.bat
+++ SQL Server dev mball.bat
@@ -1,1 +1,1 @@
-runas /noprofile /netonly /user:DEV\a-mball "C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe" 
+runas /noprofile /netonly /user:DEV\a-mball "C:\Program Files (x86)\Microsoft SQL Server\130\Tools\Binn\ManagementStudio\Ssms.exe" 
